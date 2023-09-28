751509e7fa6d641fa86cd0883fb06af870b12cad
bcavs <cavenagh.ben@gmail.com>
2023-09-27 23:26:09 -0500

    update types

--- src/server/types/index.ts
+++ src/server/types/index.ts
@@ -65,69 +65,68 @@ export type LeagueUserData = {
   display_name: string | null;
   avatar: string | null;
 } | null;
-
 export type PlayerData = {
-  id: string;
+  id?: string | null;
   // Basic player information
-  player_id: string;
-  full_name: string;
-  first_name: string;
-  last_name: string;
-  search_full_name: string;
-  search_first_name: string;
-  search_last_name: string;
-  hashtag: string;
-  sport: string;
+  player_id?: string | null;
+  full_name?: string | null;
+  first_name?: string | null;
+  last_name?: string | null;
+  search_full_name?: string | null;
+  search_first_name?: string | null;
+  search_last_name?: string | null;
+  hashtag?: string | null;
+  sport?: string | null;
 
   // Player bio
-  age: number;
-  birth_date: string;
-  birth_country: string;
-  birth_state: null | string;
-  birth_city: null | string;
-  high_school: string;
-  college: string;
+  age?: number | null;
+  birth_date?: string | null;
+  birth_country?: string | null;
+  birth_state?: string | null;
+  birth_city?: string | null;
+  high_school?: string | null;
+  college?: string | null;
 
   // Player physical attributes
-  height: string;
-  weight: string;
+  height?: string | null;
+  weight?: string | null;
 
   // Player career information
-  years_exp: number;
-  depth_chart_order: number;
-  depth_chart_position: number;
-  team: string;
-  position: string;
+  years_exp?: number | null;
+  depth_chart_order?: number | null;
+  depth_chart_position?: number | null;
+  team?: string | null;
+  position?: string | null;
 
   // Fantasy-related data
-  fantasy_positions: string[];
-  fantasy_data_id: number;
+  fantasy_positions?: string[] | null;
+  fantasy_data_id?: number | null;
 
   // Injury-related data
-  injury_status: null | string;
-  injury_start_date: null | string;
-  injury_notes: null | string;
+  injury_status?: string | null;
+  injury_start_date?: string | null;
+  injury_notes?: string | null;
 
   // Player statistics and IDs
-  stats_id: string;
-  espn_id: string;
-  sportradar_id: string;
-  rotowire_id: null | string;
-  rotoworld_id: number | null;
-  yahoo_id: null | string;
-  oddsjam_id: null | string;
-  gsis_id: null | string;
-  pandascore_id: null | string;
+  stats_id?: string | null;
+  espn_id?: string | null;
+  sportradar_id?: string | null;
+  rotowire_id?: string | null;
+  rotoworld_id?: number | null;
+  yahoo_id?: string | null;
+  oddsjam_id?: string | null;
+  gsis_id?: string | null;
+  pandascore_id?: string | null;
 
   // Player status and participation
-  status: string;
-  practice_participation: null | string;
-  practice_description: null | string;
-  active: boolean;
+  status?: string | null;
+  practice_participation?: string | null;
+  practice_description?: string | null;
+  active?: boolean | null;
 
   // Miscellaneous data
-  number: number;
-  search_rank: number;
-  news_updated: null | string;
-  metadata: null | string;
+  number?: number | null;
+  search_rank?: number | null;
+  news_updated?: string | null;
+  metadata?: string | null;
 };
