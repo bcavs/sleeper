d69953727fb0b452c451019b08e80c0c2d513174
bcavs <cavenagh.ben@gmail.com>
2024-07-24 01:28:14 -0500

    add better promotion graphic

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -33,85 +33,15 @@ export default function Home() {
                 </p>
                 <p>
                   2. The next two promotion spots are determined by the highest
-                  two regular season records.
+                  two regular season records. Tie breakers are settled by
+                  standings provided by the Sleeper App
                 </p>
                 <p>
                   3. The 4 teams with the lowest regular season records from the
                   top league will be relegated.
                 </p>
               </div>
-              <div className="flex w-full flex-col gap-4 pl-8">
-                <div>
-                  <p className="font-bold underline">Top Dawgs</p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    Champion
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    Runner up
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    Best remaining regular season record
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    Next best remaining regular season record
-                  </p>
-                  <p className="flex">
-                    <ArrowDown size={24} className="text-red-500" />
-                    5th Place
-                  </p>
-                  <p className="flex">
-                    <ArrowDown size={24} className="text-red-500" />
-                    6th Place
-                  </p>
-                  <p className="flex">
-                    <ArrowDown size={24} className="text-red-500" />
-                    7th Place
-                  </p>
-                  <p className="flex">
-                    <ArrowDown size={24} className="text-red-500" />
-                    8th Place
-                  </p>
-                </div>
-                <div>
-                  <p className="font-bold underline">Underdawgs</p>
-                  <p className="flex">
-                    <ArrowUp size={24} className="text-green-500" />
-                    Champion
-                  </p>
-                  <p className="flex">
-                    <ArrowUp size={24} className="text-green-500" />
-                    Runner up
-                  </p>
-                  <p className="flex">
-                    <ArrowUp size={24} className="text-green-500" />
-                    Best remaining regular season record
-                  </p>
-                  <p className="flex">
-                    <ArrowUp size={24} className="text-green-500" />
-                    Next best remaining regular season record
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    5th Place
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    6th Place
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    7th Place
-                  </p>
-                  <p className="flex">
-                    <Minus size={24} />
-                    8th Place
-                  </p>
-                </div>
-              </div>
+              <PromotionRelegationGraphic />
             </div>
           </div>
         </main>
@@ -119,3 +49,91 @@ export default function Home() {
     </>
   );
 }
+
+const teamsData = [
+  {
+    title: "Top Dawgs",
+    members: [
+      { name: "Deebo", icon: <Minus size={24} />, description: "Champion" },
+      { name: "John", icon: <Minus size={24} />, description: "Runner up" },
+      {
+        name: "Owen",
+        icon: <Minus size={24} />,
+        description: "Best remaining regular season record",
+      },
+      {
+        name: "Kyle",
+        icon: <Minus size={24} />,
+        description: "Next best remaining regular season record",
+      },
+      {
+        name: "Berg",
+        icon: <ArrowDown size={24} className="text-red-500" />,
+        description: "5th Place",
+      },
+      {
+        name: "Brooks",
+        icon: <ArrowDown size={24} className="text-red-500" />,
+        description: "6th Place",
+      },
+      {
+        name: "Ben",
+        icon: <ArrowDown size={24} className="text-red-500" />,
+        description: "7th Place",
+      },
+      {
+        name: "Tonner",
+        icon: <ArrowDown size={24} className="text-red-500" />,
+        description: "8th Place",
+      },
+    ],
+  },
+  {
+    title: "Underdawgs",
+    members: [
+      {
+        name: "Drake",
+        icon: <ArrowUp size={24} className="text-green-500" />,
+        description: "Champion",
+      },
+      {
+        name: "Christmas",
+        icon: <ArrowUp size={24} className="text-green-500" />,
+        description: "Runner up",
+      },
+      {
+        name: "Justin",
+        icon: <ArrowUp size={24} className="text-green-500" />,
+        description: "Best remaining regular season record",
+      },
+      {
+        name: "Keenan",
+        icon: <ArrowUp size={24} className="text-green-500" />,
+        description: "Next best remaining regular season record",
+      },
+      { name: "Layne", icon: <Minus size={24} />, description: "5th Place" },
+      { name: "Ford", icon: <Minus size={24} />, description: "6th Place" },
+      { name: "Hubble", icon: <Minus size={24} />, description: "7th Place" },
+      { name: "Moll", icon: <Minus size={24} />, description: "8th Place" },
+    ],
+  },
+];
+
+const PromotionRelegationGraphic = () => (
+  <div className="flex w-full flex-col gap-4 pl-8">
+    {teamsData.map((team, index) => (
+      <div key={team.title + index}>
+        <p className="font-bold underline">{team.title}</p>
+        {team.members.map((member, index) => (
+          <div className="flex gap-2">
+            {member.icon}
+            <p className="flex" key={index}>
+              ({member.name})
+            </p>
+            <p>{member.description}</p>
+          </div>
+        ))}
+      </div>
+    ))}
+  </div>
+);
